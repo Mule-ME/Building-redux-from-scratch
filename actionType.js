6e018234fe1900d1bbba63c737c050a91de31bbf
mulualem <mulualem@ienetworks.co>
2023-03-18 14:13:16 +0300

    Defining action types for our app

--- actionType.js
+++ actionType.js
@@ -1,4 +1,4 @@
-//Action types are just a pure string which tell us the what happened in our app
+//Action types are just a pure string which tell us the what happened in our app through the interaction with users
 //To demonstrate this concept we take a simple bug object
 const BUG_ADDED = "bugAdded";
 const BUG_RESOLVED = "bugResolved";
